3fde06618e3a86178124337fb5c505f8b4da74b7
Yen Colon <yenjcc@gmail.com>
2024-10-09 20:27:32 -0400

    Initial commit

---
+++ src/main/java/org/chefcorner/chefcorner/controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package org.chefcorner.chefcorner.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/org/chefcorner/chefcorner/ChefcornerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chefcorner.chefcorner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefcornerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

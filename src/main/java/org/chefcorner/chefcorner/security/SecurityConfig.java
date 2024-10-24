d3cc5d54cdb50c3c567a6b8123ccef342d06a999
Yen Colon <yenjcc@gmail.com>
2024-10-24 10:25:30 -0400

    Added JWT dependencies and Implements JwtUtils

--- src/main/java/org/chefcorner/chefcorner/security/SecurityConfig.java
+++ src/main/java/org/chefcorner/chefcorner/security/SecurityConfig.java
@@ -1,8 +1,9 @@
 package org.chefcorner.chefcorner.security;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,7 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SpringSecurityConfig {
+public class SecurityConfig {
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -29,5 +30,9 @@ public class SpringSecurityConfig {
 
     }
 
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
 
 }
